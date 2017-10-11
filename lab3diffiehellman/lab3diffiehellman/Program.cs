4891a83d7e4e6ae3e95f9a3bce552627b0fbb594
luispandrino <landrinomenendez@gmail.com>
2017-10-10 19:37:37 -0600

    Prueba DIffieSDES

--- lab3diffiehellman/lab3diffiehellman/Program.cs
+++ lab3diffiehellman/lab3diffiehellman/Program.cs
@@ -11,7 +11,19 @@ namespace lab3diffiehellman
     {
         static void Main(string[] args)
         {
+            DiffieHellman diffieMethods = new DiffieHellman();
+            int publicKeyA = diffieMethods.AKey(6, 23, 11);
+            int publicKeyB = diffieMethods.BKey(5, 23, 11);
 
+            int a = diffieMethods.XKey1(publicKeyB, 251, 739);
+
+            int b = diffieMethods.XKey2(publicKeyA, 87, 223);
+            Console.WriteLine("Clave pública A: " + diffieMethods.getKey(publicKeyA.ToString()));
+            Console.WriteLine("Clave pública B: " + diffieMethods.getKey(publicKeyB.ToString()));
+
+            Console.WriteLine("Clave privada: " + diffieMethods.getKey(a.ToString()));
+            Console.WriteLine("Clave privada (comprobación): " + diffieMethods.getKey(b.ToString()));
+            Console.WriteLine("");
             Console.WriteLine("Ingrese su direccion");
             string Direccion = Console.ReadLine();
             S_DES my_Des = new S_DES();
