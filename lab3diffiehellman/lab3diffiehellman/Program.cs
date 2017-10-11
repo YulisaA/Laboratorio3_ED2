f1c74b43946f0407e028562a6a0d3f0a4157de37
yulyazurdia8 <yulyazurdia8@hotmail.com>
2017-10-11 00:47:17 -0600

    Parametros agregados.

--- lab3diffiehellman/lab3diffiehellman/Program.cs
+++ lab3diffiehellman/lab3diffiehellman/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,22 +12,44 @@ namespace lab3diffiehellman
     {
         static void Main(string[] args)
         {
+            Parametros parametros = new Parametros();            
 
-            Console.WriteLine("Ingrese su direccion");
-            string Direccion = Console.ReadLine();
+
             S_DES my_Des = new S_DES();
             Cifrar Cifrado = new Cifrar();
-            Cifrado.CrearCifrado(Direccion);
-
-            Console.WriteLine("Cifrado completo");
-
-            Console.WriteLine("Ingrese su direccion2");
-            string Direccion2 = Console.ReadLine();
             Descifrar Descifrado = new Descifrar();
-            Descifrado.CrearDescifrado(Direccion2);
-            Console.WriteLine("Descifrado completo");
-            Console.ReadKey();
+            Console.WriteLine("Ingrese el comando y la dirección del archivo a cifrar:");
+            string textoIngresado = Console.ReadLine();
 
+            string Direccion = parametros.ObtenerDirección(textoIngresado);
+            string nombreArchivo = Path.GetFileNameWithoutExtension(Direccion);
+
+            if (parametros.validarComprimir(textoIngresado) == true)
+            {
+                Cifrado.CrearCifrado(Direccion, nombreArchivo);
+                Console.WriteLine("Cifrado completo.");
+            }
+            else
+            {
+                Console.WriteLine("Asegúrese de ingresar el parámetro -c para cifrar y -f antes de la ruta.");
+                Console.WriteLine("Ejemplo: -c -f\"DirecciónDelArchivo\"");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Ingrese el comando y la dirección del archivo a descifrar:");
+            string textoIngresado2 = Console.ReadLine();
+            string Direccion2 = parametros.ObtenerDirección(textoIngresado2);
+
+            if (parametros.validarDescomprimir(textoIngresado2) == true)
+            {            
+                Descifrado.CrearDescifrado(Direccion2);
+                Console.WriteLine("Descifrado completo.");
+            }
+            else
+            {
+                Console.WriteLine("Asegúrese de ingresar el parámetro -d para descifrar y -f antes de la ruta.");
+                Console.WriteLine("Ejemplo: -d -f\"DirecciónDelArchivo\"");
+            }
             Console.ReadLine();
         }
 
